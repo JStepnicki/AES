ab518082ea858dc8dd5855ec139d38858ef7153c
JStepnicki <j.stepnicki2002@gmail.com>
2023-04-06 21:49:39 +0200

    pierwszy comimt

--- AES/AES.java
+++ AES/AES.java
@@ -1,2 +1,15 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class AES {
+    public byte[] generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator key = KeyGenerator.getInstance("AES");
+        SecureRandom random = new SecureRandom();
+        key.init(128,random);
+        SecretKey secretKey = key.generateKey();
+        byte [] key_in_bytes = secretKey.getEncoded();
+        return key_in_bytes;
+    }
 }
