abb7104ef95b6ef759da33d5c0cae7629249b5a3
umbc1ok <umbcio@gmail.com>
2023-04-12 22:55:40 +0200

    Dziala essa jak tralala

--- AES/view/src/main/java/com/example/view/HelloController.java
+++ AES/view/src/main/java/com/example/view/HelloController.java
@@ -3,15 +3,22 @@ package com.example.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class HelloController {
 
-    AES dupa = new AES();
+    private AES aes = new AES();
+    private FileChooser fileChooser = new FileChooser();
 
     //final byte[] keyInBytes = dupa.generateKey(128);
     @FXML
@@ -26,6 +33,15 @@ public class HelloController {
     @FXML
     private TextArea cipheredText;
 
+    @FXML
+    private javafx.scene.control.Button loadNormalText;
+
+    @FXML
+    private javafx.scene.control.Button loadCipheredText;
+
+    @FXML
+    private javafx.scene.control.Button saveToFileButton;
+
     public HelloController() throws NoSuchAlgorithmException {
     }
 
@@ -37,19 +53,19 @@ public class HelloController {
 
     @FXML
     protected void generateKey128() throws NoSuchAlgorithmException {
-        byte[] temp = dupa.generateKey(128);
+        byte[] temp = aes.generateKey(128);
         String base64String = Base64.getEncoder().encodeToString(temp);
         keyField.setText(base64String);
     }
     @FXML
     protected void generateKey192() throws NoSuchAlgorithmException {
-        byte[] temp = dupa.generateKey(192);
+        byte[] temp = aes.generateKey(192);
         String base64String = Base64.getEncoder().encodeToString(temp);
         keyField.setText(base64String);
     }
     @FXML
     protected void generateKey256() throws NoSuchAlgorithmException {
-        byte[] temp = dupa.generateKey(256);
+        byte[] temp = aes.generateKey(256);
         String base64String = Base64.getEncoder().encodeToString(temp);
         keyField.setText(base64String);
     }
@@ -60,7 +76,7 @@ public class HelloController {
         String key = keyField.getText();
         byte[] messageInBytes = toEncrypt.getBytes(StandardCharsets.UTF_8);
         byte[] keyInBytes = Base64.getDecoder().decode(key);
-        byte[] encryptedMessageInBytes = Base64.getEncoder().encode(dupa.encryptMessage(messageInBytes,keyInBytes));
+        byte[] encryptedMessageInBytes = Base64.getEncoder().encode(aes.encryptMessage(messageInBytes,keyInBytes));
         String encryptedMessage = new String(encryptedMessageInBytes,StandardCharsets.UTF_8);
         cipheredText.setText(encryptedMessage);
     }
@@ -71,9 +87,67 @@ public class HelloController {
         String key = keyField.getText();
         byte[] messageInBytes = Base64.getDecoder().decode(toDecrypt.getBytes(StandardCharsets.UTF_8));
         byte[] keyInBytes = Base64.getDecoder().decode(key);
-        byte[] decryptedMessageInbytes = dupa.decryptMessage(messageInBytes,keyInBytes);
+        byte[] decryptedMessageInbytes = aes.decryptMessage(messageInBytes,keyInBytes);
         String decryptedMessage = new String(decryptedMessageInbytes,StandardCharsets.UTF_8);
         normalText.setText(decryptedMessage);
+    }
+
+    @FXML
+    protected void readFromFileNormal() throws Exception {
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        File file = fileChooser.showOpenDialog(null);
+        normalText.setText(fileInputMessage(file.getPath()));
+    }
+
+    @FXML
+    protected void readFromFileEncrypted() throws Exception {
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        File file = fileChooser.showOpenDialog(null);
+        cipheredText.setText(fileInputMessage(file.getPath()));
+    }
+
+    public  String fileInputMessage(String filePath) throws Exception {
+        Path path = Paths.get(filePath);
+        String content = Files.readString(path, StandardCharsets.UTF_8);
+        return content;
+    }
+
+    @FXML
+    protected void saveToFile(){
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        File file = fileChooser.showOpenDialog(null);
+        saveToFileHelper(file.getPath(),keyField.getText(),cipheredText.getText(),normalText.getText());
+    }
+
+    @FXML
+    protected void saveEncryptedToFile(){
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        File file = fileChooser.showOpenDialog(null);
+        saveEncryptedToFileHelper(file.getPath(),cipheredText.getText());
+    }
+
+    public void saveToFileHelper(String filePath,String cipherKey,String encryptedMessage,String decryptedMessage){
+        String toSave = "uzyty klucz: " +  cipherKey + "\n" + "wiadomosc po zaszyfrowaniu: " +encryptedMessage + "\n wiadomość po deszyfracji: "+decryptedMessage ;
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write(toSave);
+            writer.close();
+            System.out.println("Zapisano dane do pliku " + filePath);
+        } catch (IOException e) {
+            System.out.println("Błąd zapisu do pliku " + filePath + ": " + e.getMessage());
+        }
+    }
+
+    public void saveEncryptedToFileHelper(String filePath,String encryptedMessage){
+        String toSave = encryptedMessage;
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write(toSave);
+            writer.close();
+            System.out.println("Zapisano dane do pliku " + filePath);
+        } catch (IOException e) {
+            System.out.println("Błąd zapisu do pliku " + filePath + ": " + e.getMessage());
+        }
     }
 
 
