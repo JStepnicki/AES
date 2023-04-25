48a5819edc7a5f6eab413ff40dbab473668c6160
umbc1ok <umbcio@gmail.com>
2023-04-25 19:42:05 +0200

    Fixed GUI

--- AES/view/src/main/java/com/example/view/Application.java
+++ AES/view/src/main/java/com/example/view/Application.java
@@ -10,8 +10,8 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1800, 900);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 400);
+        stage.setTitle("AES Encryption and Decryption");
         stage.setScene(scene);
         stage.show();
     }
--- AES/view/src/main/java/com/example/view/ApplicationController.java
+++ AES/view/src/main/java/com/example/view/ApplicationController.java
@@ -1,6 +1,7 @@
 package com.example.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
@@ -50,37 +51,70 @@ public class ApplicationController {
 
     @FXML
     public void encryptBinaryFile() throws Exception {
-        //fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("WARNING");
+        alert.setHeaderText(null);
+        alert.setContentText("You are going to choose which file you want to encrypt. Make sure you have your key saved. If you lose it you will not be able to decrypt the file.");
+
+
+        alert.showAndWait();
         File file = fileChooser.showOpenDialog(null);
         String sciezka = file.getPath();
         byte[] content = Files.readAllBytes(Paths.get(sciezka));
         String keyHelper = keyField.getText();
         byte[] key = Base64.getDecoder().decode(keyHelper);
-
-        byte[] encryptedMessage = aes.encryptMessage(content,key);
-        File file2 = fileChooser.showOpenDialog(null);
-        OutputStream outputStream = new FileOutputStream(file2.getPath());
-        outputStream.write(encryptedMessage, 0,encryptedMessage.length);
+        byte[] encryptedMessage;
+        try {
+            encryptedMessage = aes.encryptMessage(content, key);
+        } catch (Exception e) {
+            Alert alert3 = new Alert(Alert.AlertType.INFORMATION);
+            alert3.setTitle("Done!");
+            alert3.setHeaderText(null);
+            alert3.setContentText("The key was invalid");
+            alert3.showAndWait();
+            return;
+        }
+        OutputStream outputStream = new FileOutputStream(file.getPath());
+        outputStream.write(encryptedMessage, 0, encryptedMessage.length);
         outputStream.close();
-        System.out.println("AAAAAAAES");
+        Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+        alert2.setTitle("Done!");
+        alert2.setHeaderText(null);
+        alert2.setContentText("The file has been encrypted. Remember to save your key!");
+        alert2.showAndWait();
     }
 
     @FXML
     public void decryptBinaryFile() throws Exception {
-        AES szyfr =  new AES();
         String key = keyField.getText();
         byte[] keyInBytes = Base64.getDecoder().decode(key);
+
 
         File file = fileChooser.showOpenDialog(null);
         String sciezka = file.getPath();
         byte[] encryptedFromFile = Files.readAllBytes(Paths.get(sciezka));
-        byte[] decryptedMessage = aes.decryptMessage(encryptedFromFile,keyInBytes);
+        byte[] decryptedMessage;
+        try{
+            decryptedMessage = aes.decryptMessage(encryptedFromFile,keyInBytes);
+        }
+        catch(Exception e){
+            Alert alert3 = new Alert(Alert.AlertType.INFORMATION);
+            alert3.setTitle("Done!");
+            alert3.setHeaderText(null);
+            alert3.setContentText("The key was invalid");
+            alert3.showAndWait();
+            return;
+        }
 
-        File file1 = fileChooser.showOpenDialog(null);
 
-        OutputStream outputStream2 = new FileOutputStream(file1.getPath());
+        OutputStream outputStream2 = new FileOutputStream(file.getPath());
         outputStream2.write(decryptedMessage, 0,decryptedMessage.length);
         outputStream2.close();
+        Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+        alert2.setTitle("Done!");
+        alert2.setHeaderText(null);
+        alert2.setContentText("The file has been decrypted.");
+        alert2.showAndWait();
     }
 
 }
